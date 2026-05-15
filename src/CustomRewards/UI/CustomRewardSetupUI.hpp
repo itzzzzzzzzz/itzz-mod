6cf282bb339f94c013d7c018e40c7d6071a61d0d
itzz <georg.ott.mail@gmail.com>
2026-05-15 19:27:41 +0200

    cooking

---
+++ src/CustomRewards/UI/CustomRewardSetupUI.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include "../../GUI/BackgroundSprite.hpp"
+#include "../../GUI/BetterButtonSprite.hpp"
+#include "../../Utils/AdvancedLabel/AdvLabelBMFont.hpp"
+#include "../../GUI/PopupBase.hpp"
+
+using namespace geode::prelude;
+
+class CustomRewardSetupUI : public PopupBase
+{
+    protected:
+        qolmod::BackgroundSprite* bg;
+        std::unordered_map<std::string, TextInput*> textInputs = {};
+
+    public:
+        static CustomRewardSetupUI* create();
+
+        CCNode* createCreateInput(std::string id, std::string str);
+
+        void onCreateReward(CCObject* sender);
+
+        virtual bool setup();
+};

d8e502811926ccd18bdbb818a335e2943b622275
itzz <georg.ott.mail@gmail.com>
2026-05-15 13:37:47 +0200

    cooking

---
+++ src/CustomRewards/CustomRewards.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../Client/ButtonModule.hpp"
+
+class CreateCustomReward : public ButtonModule
+{
+    public:
+        MODULE_SETUP(CreateCustomReward)
+        {
+            setID("create-custom-reward");
+            setCategory("Shortcuts");
+
+            setPriority(sortPriority + 1);
+        }
+
+        virtual void onClick();
+};
+
+SUBMIT_HACK(CreateCustomReward);

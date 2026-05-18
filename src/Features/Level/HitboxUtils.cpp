3fd047373e7b9d3baeee1603d934bd5aa336d528
itzz <georg.ott.mail@gmail.com>
2026-05-18 04:54:41 +0200

    cooking

---
+++ src/Features/Level/HitboxUtils.cpp
@@ -0,0 +1,36 @@
+#include "HitboxUtils.hpp"
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+bool HitboxUtils::shouldHitboxesBeVisible()
+{
+    if (auto gjbgl = GJBaseGameLayer::get())
+    {
+        if (gjbgl->m_isDebugDrawEnabled)
+        {
+            if (auto pl = typeinfo_cast<PlayLayer*>(gjbgl))
+            {
+                if (pl->m_isPracticeMode)
+                    return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        
+        #if GEODE_COMP_GD_VERSION >= 22081
+        if (gjbgl->m_hitboxesOnDeath)
+        {
+            if (gjbgl->m_player1 && gjbgl->m_player1->m_isDead)
+                return true;
+
+            if (gjbgl->m_player2 && gjbgl->m_player2->m_isDead)
+                return true;
+        }
+        #endif
+    }
+
+    return false;
+}

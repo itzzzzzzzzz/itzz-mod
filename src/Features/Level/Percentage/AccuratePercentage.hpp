5d3a60e6c0e6effbf5a4dfdcc5bab107ca09d6cd
itzz <georg.ott.mail@gmail.com>
2026-05-20 01:10:03 +0200

    cooking

---
+++ src/Features/Level/Percentage/AccuratePercentage.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "../../../Client/InputModule.hpp"
+#include "../../Utils/PlayLayer.hpp"
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+class AccuratePercentage : public Module
+{
+    public:
+        MODULE_SETUP(AccuratePercentage)
+        {
+            setName("Accurate Percentage");
+            setID("accurate-percentage");
+            setCategory("Level");
+            setDescription("Allows you to have any amount of decimal places in your percentage text");
+        }
+};
+
+class AccuratePercentagePlaces : public InputModule
+{
+    public:
+        MODULE_SETUP(AccuratePercentagePlaces)
+        {
+            setName("Places");
+            setID("accurate-percentage/places");
+            setDescription("");
+
+            setDefaultString("2");
+            setPlaceholderString("Places");
+
+            setStringFilter("1234567890");
+            setMaxCharCount(5);
+        }
+};
+
+SUBMIT_HACK(AccuratePercentage);
+SUBMIT_OPTION(AccuratePercentage, AccuratePercentagePlaces);

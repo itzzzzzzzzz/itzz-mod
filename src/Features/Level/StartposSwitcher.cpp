0797f2dfd84f5d2eadd7d40591ee963675011fa4
itzz <georg.ott.mail@gmail.com>
2026-05-21 03:09:25 +0200

    cooking

---
+++ src/Features/Level/StartposSwitcher.cpp
@@ -0,0 +1,207 @@
+#include "../../Client/KeybindModule.hpp"
+#include "../../Client/LayoutModule.hpp"
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/UILayer.hpp>
+#include "../../GUI/StartposSwitcherUI.hpp"
+#include "../Utils/PlayLayer.hpp"
+
+using namespace geode::prelude;
+
+class StartposSwitcher : public Module
+{
+    public:
+        MODULE_SETUP(StartposSwitcher)
+        {
+            setName("Startpos Switcher");
+            setID("startpos-switcher");
+            setDefaultEnabled(true);
+            setCategory("Level");
+            setDescription("Switch between start positions in the level at any time");
+            setPriority(5);
+        }
+};
+
+class StartposSwitcherPrev : public KeybindModule
+{
+    public:
+        MODULE_SETUP(StartposSwitcherPrev)
+        {
+            setName("Prev:");
+            setID("startpos-switcher/prev-bind");
+            setDescription("");
+            setPriority(1);
+
+            setDefaultKeyCode(81);
+        }
+};
+
+class StartposSwitcherNext : public KeybindModule
+{
+    public:
+        MODULE_SETUP(StartposSwitcherNext)
+        {
+            setName("Next:");
+            setID("startpos-switcher/next-bind");
+            setDescription("");
+            setPriority(2);
+
+            setDefaultKeyCode(69); // hehe
+        }
+};
+
+class StartposSwitcherHide : public Module
+{
+    public:
+        MODULE_SETUP(StartposSwitcherHide)
+        {
+            setName("Hide UI");
+            setID("startpos-switcher/hide-ui");
+            setDescription("Hides the UI and makes it non-interactable");
+            setPriority(3);
+        }
+};
+
+class StartposSwitcherPercentage : public Module
+{
+    public:
+        MODULE_SETUP(StartposSwitcherPercentage)
+        {
+            setName("Use Percentage");
+            setID("startpos-switcher/use-percentage");
+            setDescription("Shows the percentage that each start position is at");
+            setPriority(4);
+        }
+};
+
+class StartposSwitcherLayout : public LayoutModule
+{
+    public:
+        MODULE_SETUP(StartposSwitcherLayout)
+        {
+            setID("startpos-switcher/layout");
+            setPriority(5);
+            setDefaults(Anchor::Bottom, ccp(0, 35));
+
+            setPreviewNode([]{
+                return StartposSwitcherUI::create(false);
+            });
+        }
+};
+
+SUBMIT_HACK(StartposSwitcher);
+SUBMIT_OPTION(StartposSwitcher, StartposSwitcherPrev);
+SUBMIT_OPTION(StartposSwitcher, StartposSwitcherNext);
+SUBMIT_OPTION(StartposSwitcher, StartposSwitcherHide);
+SUBMIT_OPTION(StartposSwitcher, StartposSwitcherPercentage);
+SUBMIT_OPTION(StartposSwitcher, StartposSwitcherLayout);
+
+class $modify (StartposPlayLayer, PlayLayer)
+{
+    struct Fields
+    {
+        StartposSwitcherUI* ui = nullptr;
+        int startPosIndex = 0;
+    };
+
+    void createObjectsFromSetupFinished()
+    {
+        PlayLayer::createObjectsFromSetupFinished();
+
+        m_fields->ui = StartposSwitcherUI::create();
+        StartposSwitcherLayout::get()->applyToNode(m_fields->ui);
+
+        m_fields->ui->onSwitchFunc = [this](int switchBy)
+        {
+            this->switchStartPosByAmount(switchBy);
+        };
+
+        if (m_uiLayer)
+            m_uiLayer->addChild(m_fields->ui);
+
+        if (StartposSwitcher::get()->getRealEnabled())
+            setStartPos(0);
+    }
+
+    void switchStartPosByAmount(int amount)
+    {
+        setStartPos(m_fields->startPosIndex + amount);
+    }
+
+    void setStartPos(int index)
+    {
+        if (!PlayLayerUtils::getUtils())
+            return;
+
+        if (PlayLayerUtils::getUtils()->m_fields->startPositions.empty())
+            return;
+
+        if (index < 0)
+            index = PlayLayerUtils::getUtils()->m_fields->startPositions.size();
+
+        if (index > PlayLayerUtils::getUtils()->m_fields->startPositions.size())
+            index = 0;
+
+        m_fields->startPosIndex = index;
+
+        m_currentCheckpoint = nullptr;
+
+        auto spos = index == 0 ? nullptr : PlayLayerUtils::getUtils()->m_fields->startPositions[m_fields->startPosIndex - 1];
+
+        setStartPosObject(spos);
+
+        if (isRunning())
+        {
+            if (m_isPracticeMode)
+                resetLevelFromStart();
+
+            PlayLayer::resetLevel();
+            startMusic();
+        }
+
+        m_fields->ui->setStartposVisual(m_fields->startPosIndex);
+    }
+
+    void postUpdate(float p0)
+    {
+        PlayLayer::postUpdate(p0);
+
+        if (m_fields->ui)
+        {
+            m_fields->ui->setVisible(StartposSwitcher::get()->getRealEnabled() && !StartposSwitcherHide::get()->getRealEnabled());
+            m_fields->ui->updateKeybindsVisualizer(false, StartposSwitcherPrev::get()->getKeyCode(), StartposSwitcherNext::get()->getKeyCode());
+            m_fields->ui->usePercentage = StartposSwitcherPercentage::get()->getRealEnabled();
+            m_fields->ui->updateUI();
+        }
+    }
+};
+
+class $modify (StartposUILayer, UILayer)
+{
+    #if GEODE_COMP_GD_VERSION >= 22081
+    void handleKeypress(cocos2d::enumKeyCodes key, bool down, double timestamp)
+    #else
+    void handleKeypress(cocos2d::enumKeyCodes key, bool down)
+    #endif
+    {
+        if (down && StartposSwitcher::get()->getRealEnabled())
+        {
+            if (auto pl = static_cast<StartposPlayLayer*>(PlayLayer::get()))
+            {
+                if (!pl->m_levelEndAnimationStarted)
+                {
+                    if (key == StartposSwitcherPrev::get()->getKeyCode())
+                        pl->switchStartPosByAmount(-1);
+
+                    if (key == StartposSwitcherNext::get()->getKeyCode())
+                        pl->switchStartPosByAmount(1);
+                }
+            }
+        }
+
+        #if GEODE_COMP_GD_VERSION >= 22081
+        UILayer::handleKeypress(key, down, timestamp);
+        #else
+        UILayer::handleKeypress(key, down);
+        #endif
+    }
+};

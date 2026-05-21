c7948a5bca3c39b1d11f843eefacc4a8f128331d
itzz <georg.ott.mail@gmail.com>
2026-05-21 09:14:50 +0200

    cooking

---
+++ src/Features/Speedhack/Hooks.cpp
@@ -0,0 +1,85 @@
+#include "Hooks.hpp"
+#include "Speedhack.hpp"
+#include "../../Utils/ColourUtils.hpp"
+#include "../../Labels/LabelManager.hpp"
+
+Mod* cbf = nullptr;
+
+bool CBFCheckMenuLayer::init()
+{
+    cbf = Loader::get()->getLoadedMod("syzzi.click_between_frames");
+    return MenuLayer::init();
+}
+
+void SpeedhackScheduler::update(float dt)
+{
+    Speedhack::get()->realDeltatime = dt;
+    ColourUtils::get()->update(dt);
+    LabelManager::get()->update(dt);
+
+    float value = Speedhack::get()->getRealValue();
+
+    static float lastPitch = 0;
+    float pi = (Speedhack::get()->getMusicEnabled() && Speedhack::get()->gameplayOnlyCheck()) ? value : 1;
+
+    if (lastPitch != pi)
+    {
+        // itzz: null-sicher -> kein Crash wenn FMOD-Channel nicht bereit
+        if (auto ch = Speedhack::get()->getMasterChannel())
+            ch->setPitch(pi);
+
+        lastPitch = pi;
+    }
+
+    if (Speedhack::get()->getGameplayEnabled())
+    {
+        CCScheduler::update(dt);
+        
+        return;
+    }
+
+    if (cbf)
+    {
+        auto director = CCDirector::get();
+
+        director->m_fActualDeltaTime *= value;
+        director->m_fDeltaTime *= value;
+    }
+
+    CCScheduler::update(dt * value);
+}
+
+bool isNodeUnpausable(CCNode* node)
+{
+    if (!node)
+        return false;
+
+    if (node->getUserObject("unpausable"_spr))
+        return true;
+
+    if (node->getParent())
+        return isNodeUnpausable(node->getParent());
+
+    return false;
+}
+
+void SpeedhackScheduler::pauseTarget(CCObject *pTarget)
+{
+    if (auto node = typeinfo_cast<CCNode*>(pTarget))
+    {
+        if (isNodeUnpausable(node))
+            return;
+    }
+
+    CCScheduler::pauseTarget(pTarget);
+}
+
+void SpeedhackBaseGameLayer::update(float dt)
+{
+    if (Speedhack::get()->getGameplayEnabled())
+    {
+        dt *= Speedhack::get()->getRealValue();
+    }
+
+    GJBaseGameLayer::update(dt);
+}
---
+++ src/Features/Speedhack/Hooks.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include <Geode/modify/CCScheduler.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify (CBFCheckMenuLayer, MenuLayer)
+{
+    virtual bool init();
+};
+
+class $modify (SpeedhackScheduler, CCScheduler)
+{
+    virtual void update(float dt);
+
+    void pauseTarget(CCObject *pTarget);
+};
+
+class $modify (SpeedhackBaseGameLayer, GJBaseGameLayer)
+{
+    virtual void update(float dt);
+};

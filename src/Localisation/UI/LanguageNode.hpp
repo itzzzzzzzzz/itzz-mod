439705938beade6c0a6f64f5c9549a548111f3cd
itzz <georg.ott.mail@gmail.com>
2026-06-12 18:51:31 +0200

    cooking

---
+++ src/Localisation/UI/LanguageNode.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include "../CLanguage.hpp"
+#include <FallbackLabel.hpp>
+
+using namespace geode::prelude;
+
+class LanguageNode : public CCNode
+{
+    protected:
+        std::string lang = "en-AU.json";
+        CLanguage* language = nullptr;
+        
+        CCScale9Sprite* bg;
+        CCLabelBMFont* percentLabel;
+        CCLabelBMFont* enName;
+        FallbackLabel* nativeName;
+        CCMenu* useMenu = nullptr;
+
+    public:
+        static LanguageNode* create(std::string lang);
+
+        void onSelectLanguage(CCObject* sender);
+        void onViewCredits(CCObject* sender);
+        void onMissingTranslations(CCObject* sender);
+
+        float getPercentageComplete();
+
+        virtual void visit();
+
+        bool init(std::string lang);
+};

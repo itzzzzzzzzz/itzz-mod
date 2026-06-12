6f56e50173cb8bf4fc17a9e048617855bc2e05f1
itzz <georg.ott.mail@gmail.com>
2026-06-12 07:40:05 +0200

    cooking

---
+++ src/Localisation/LocalisationManager.cpp
@@ -0,0 +1,234 @@
+#include "LocalisationManager.hpp"
+#include "../Utils/AdvancedLabel/AdvLabelBMFont.hpp"
+#include "../GUI/AndroidUI.hpp"
+#include <FastBMFontConfig.hpp>
+
+using namespace geode::prelude;
+
+LocalisationManager* LocalisationManager::get()
+{
+    static LocalisationManager* instance = nullptr;
+
+    if (!instance)
+    {
+        instance = new LocalisationManager();
+        instance->setup();
+    }
+
+    return instance;
+}
+
+void LocalisationManager::setup()
+{
+    placeholder = CLanguage::createWithJSON("{}");
+
+    for (auto file : getAllLanguageFilesPath())
+    {
+        auto data = file::readJson(file);
+
+        if (data.isOk())
+        {
+            auto lang = CLanguage::createWithJSON(data.unwrap());
+
+            languages.emplace(file, lang);
+        }
+    }
+
+    auto path = Mod::get()->getResourcesDir() / Mod::get()->getSavedValue<std::string>("loaded-localisation-file", "en-AU.json");
+
+    if (std::filesystem::exists(path))
+    {
+        LocalisationManager::get()->loadLocalisationFile(path);
+    }
+    else
+    {
+        LocalisationManager::get()->loadLocalisationFile(Mod::get()->getResourcesDir() / "en-AU.json");
+    }
+}
+
+void LocalisationManager::loadLocalisationFile(std::filesystem::path path)
+{
+    currentLang = nullptr;
+
+    if (languages.contains(path))
+        currentLang = languages[path];
+
+    currentPath = path;
+}
+
+std::filesystem::path LocalisationManager::getCurrentLoadedFile()
+{
+    return currentPath;
+}
+
+void LocalisationManager::switchFinished()
+{
+    if (AndroidUI::get())
+    {
+        AndroidUI::get()->close();
+        AndroidUI::addToScene()->playAnimation(qolmod::MenuAnimation::None);
+    }
+}
+
+void LocalisationManager::switchLocalisationWithUIPath(std::filesystem::path path)
+{
+    loadLocalisationFile(path);
+    auto font = LocalisationManager::get()->getAltFont();
+
+    if (!font.empty() && !CCTextureCache::get()->textureForKey(utils::string::replace(font, ".fnt", ".png").c_str()))
+    {
+        auto layer = CCLayerColor::create(ccc4(0, 0, 0, 150));
+
+        auto lbl = CCLabelBMFont::create("Loading language font", "bigFont.fnt");
+        lbl->setScale(0.65f);
+        lbl->setPosition(layer->getContentSize() / 2);
+
+        layer->addChild(lbl);
+        CCScene::get()->addChild(layer, CCScene::get()->getHighestChildZ() + 2);
+        
+        Loader::get()->queueInMainThread([this, layer, path, font]
+        {
+            Loader::get()->queueInMainThread([this, layer, path, font]
+            {
+                Loader::get()->queueInMainThread([this, layer, path, font]
+                {
+                    FastBMFontConfig::quickLoad(font.c_str());
+
+                    layer->removeFromParent();
+
+                    switchFinished();
+                });
+            });
+        });
+
+        return;
+    }
+
+    switchFinished();
+}
+
+void LocalisationManager::switchLocalisationWithUI(std::string file)
+{
+    Mod::get()->setSavedValue<std::string>("loaded-localisation-file", file);
+
+    switchLocalisationWithUIPath(Mod::get()->getResourcesDir() / file);
+}
+
+AdvLabelTTFUsage LocalisationManager::getDefaultTTFUsage()
+{
+    if (currentLang)
+    {
+        switch (currentLang->getFontUsageMode())
+        {
+            case 0:
+                return AdvLabelTTFUsage::Forced;
+
+            case 1:
+                return AdvLabelTTFUsage::Auto;
+
+            case 2:
+                return AdvLabelTTFUsage::None;
+        }
+    }
+    
+    return AdvLabelTTFUsage::Auto;
+}
+
+std::string LocalisationManager::getLocalisedString(std::string id)
+{
+    auto errorStr = fmt::format("<ttf>{}", id);
+    
+    if (currentLang && currentLang->containsKey(id))
+        return currentLang->stringForKey(id);
+
+    return errorStr;
+}
+
+CLanguage* LocalisationManager::languageForPath(std::filesystem::path path)
+{
+    if (languages.contains(path))
+        return languages[path];
+
+    return placeholder;
+}
+
+std::string LocalisationManager::getAltFont()
+{
+    if (!currentLang)
+        return "notosans.fnt"_spr;
+
+    return currentLang->getFont();
+}
+
+std::vector<std::string> LocalisationManager::getAllLanguageFiles()
+{
+    std::vector<std::string> files = {};
+
+    for (auto file : std::filesystem::directory_iterator(Mod::get()->getResourcesDir()))
+    {
+        auto p = file.path().filename();
+
+        if (p.has_extension() && p.extension().string() == ".json")
+        {
+            files.push_back(file.path().filename().string());
+        }
+    }
+
+    return files;
+}
+
+std::vector<std::filesystem::path> LocalisationManager::getAllLanguageFilesPath()
+{
+    std::vector<std::filesystem::path> files = {};
+
+    for (auto file : std::filesystem::directory_iterator(Mod::get()->getResourcesDir()))
+    {
+        auto p = file.path().filename();
+
+        if (p.has_extension() && p.extension().string() == ".json")
+        {
+            files.push_back(file);
+        }
+    }
+
+    return files;
+}
+
+CLanguage* LocalisationManager::getCurrentLang()
+{
+    return currentLang ? currentLang : placeholder;
+}
+
+#include "ShapingEngine.hpp"
+#include <Utils.hpp>
+
+std::string LocalisationManager::reshapeArabicString(std::string str)
+{
+#ifdef GEODE_IS_MOBILE
+    return str; // mobile renders arabic correctly with CCLabelTTF
+#endif
+
+    auto ws = qolmod::utils::toWideString(str);
+    
+    return ShapingEngine::render(ws, true);
+}
+
+void LocalisationManager::switchTempPath(std::filesystem::path path)
+{
+    if (languages.contains(path))
+        CC_SAFE_DELETE(languages[path]);
+
+    auto data = file::readJson(path);
+
+    if (data.isOk())
+    {
+        auto lang = CLanguage::createWithJSON(data.unwrap());
+
+        if (languages.contains(path))
+            languages[path] = lang;
+        else
+            languages.emplace(path, lang);
+    }
+
+    switchLocalisationWithUIPath(path);
+}
---
+++ src/Localisation/LocalisationManager.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include "../Utils/AdvancedLabel/AdvLabelBMFont.hpp"
+
+#include "CLanguage.hpp"
+
+class LocalisationManager
+{
+    protected:
+        std::unordered_map<std::filesystem::path, CLanguage*> languages = {};
+        CLanguage* currentLang = nullptr;
+        CLanguage* placeholder = nullptr;
+        std::filesystem::path currentPath;
+
+        void loadLocalisationFile(std::filesystem::path path);
+        void setup();
+
+    public:
+        static LocalisationManager* get();
+
+        void switchTempPath(std::filesystem::path path);
+        void switchLocalisationWithUIPath(std::filesystem::path path);
+        void switchLocalisationWithUI(std::string file);
+
+        void switchFinished();
+
+        AdvLabelTTFUsage getDefaultTTFUsage();
+
+        std::vector<std::string> getAllLanguageFiles();
+        std::vector<std::filesystem::path> getAllLanguageFilesPath();
+
+        std::filesystem::path getCurrentLoadedFile();
+        std::string getAltFont();
+        std::string getLocalisedString(std::string id);
+        std::string reshapeArabicString(std::string str);
+
+        CLanguage* languageForPath(std::filesystem::path path);
+        CLanguage* getCurrentLang();
+};

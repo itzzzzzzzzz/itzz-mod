cb31d3a7098c08e44eb8184b3d8cd57d0b2a0cb2
itzz <georg.ott.mail@gmail.com>
2026-06-14 23:20:38 +0200

    cooking

---
+++ src/SafeMode/Modules/SafeMode.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "../../Client/InputModule.hpp"
+#include "SafeMode.hpp"
+#include "DisableCheatsInMenu.hpp"
+
+using namespace geode::prelude;
+
+class ForceSafeMode : public Module
+{
+    public:
+        MODULE_SETUP(ForceSafeMode)
+        {
+            setName("Safe Mode");
+            setID("safe-mode");
+            setCategory("Universal");
+            setDescription("Disables all level progress and stats");
+            setPriority(2);
+        }
+};
+
+class SafeModeChangeText : public Module
+{
+    public:
+        MODULE_SETUP(SafeModeChangeText)
+        {
+            setName("Change Text");
+            setID("safe-mode/change-text");
+            setDescription("Changes the end screen text to make it obvious if the run is cheated");
+            setDefaultEnabled(true);
+            setPriority(1);
+        }
+};
+
+class SafeModeText : public InputModule
+{
+    public:
+        MODULE_SETUP(SafeModeText)
+        {
+            setName("Text");
+            setID("safe-mode/text");
+            
+            setDefaultString("Safe Mode :3");
+            setPlaceholderString("Text");
+
+            setStringFilter("");
+            setMaxCharCount(50);
+            setPriority(2);
+        }
+};
+
+class SafeModeEndScreenCheatIndicator : public Module
+{
+    public:
+        MODULE_SETUP(SafeModeEndScreenCheatIndicator)
+        {
+            setID("safe-mode/end-screen-cheat-indicator");
+            setDefaultEnabled(true);
+            setPriority(3);
+        }
+};
+
+SUBMIT_HACK(ForceSafeMode);
+SUBMIT_OPTION(ForceSafeMode, SafeModeChangeText);
+SUBMIT_OPTION(ForceSafeMode, SafeModeText);
+SUBMIT_OPTION(ForceSafeMode, SafeModeEndScreenCheatIndicator);
+SUBMIT_OPTION(ForceSafeMode, SafeModeDisableCheats);
---
+++ src/SafeMode/SafeMode.cpp
@@ -0,0 +1,137 @@
+#include "SafeMode.hpp"
+#include "Modules/AutoSafeMode.hpp"
+#include "Modules/SafeMode.hpp"
+#include "../Features/Speedhack/Speedhack.hpp"
+
+SafeMode* SafeMode::get()
+{
+    static SafeMode* instance = nullptr;
+
+    if (!instance)
+        instance = new SafeMode();
+
+    return instance;
+}
+
+void SafeMode::onLevelLoad()
+{
+    isLevelLoadCheated = false;
+    levelLoadReasons.clear();
+
+    for (auto module : Module::getAll())
+    {
+        if (module->getSafeModeTrigger() == SafeModeTrigger::LevelLoad)
+        {
+            if (module->getRealEnabled())
+            {
+                isLevelLoadCheated = true;
+                addMessage(module->getSafeModeTrigger(), fmt::format("<cc>{}</c> enabled on <co>level load</c>", module->getName()));
+            }
+        }
+    }
+
+    onLevelNewAttempt();
+}
+
+void SafeMode::onLevelNewAttempt()
+{
+    isAttemptCheated = false;
+    attemptReasons.clear();
+
+    for (auto module : Module::getAll())
+    {
+        if (module->getSafeModeTrigger() == SafeModeTrigger::Attempt)
+        {
+            if (module->getRealEnabled())
+            {
+                isAttemptCheated = true;
+                addMessage(module->getSafeModeTrigger(), fmt::format("<cc>{}</c> enabled for <co>this attempt</c>", module->getName()));
+            }
+        }
+    }
+
+    onSpeedhackChanged();
+}
+
+void SafeMode::onModuleToggled(Module* mod)
+{
+    if (mod->getSafeModeTrigger() == SafeModeTrigger::Attempt)
+    {
+        isAttemptCheated = true;
+        addMessage(mod->getSafeModeTrigger(), fmt::format("<cc>{}</c> enabled for <co>this attempt</c>", mod->getName()));
+    }
+}
+
+void SafeMode::onSpeedhackChanged()
+{
+    if (Speedhack::get()->getRealValue() < 1)
+    {
+        isAttemptCheated = true;
+        addMessage(SafeModeTrigger::Attempt, "<cc>Speedhack</c> enabled for <co>this attempt</c>");
+    }
+}
+
+void SafeMode::addMessage(SafeModeTrigger level, std::string message)
+{
+    switch (level)
+    {
+        case SafeModeTrigger::Attempt:
+            if (std::find(attemptReasons.begin(), attemptReasons.end(), message) == attemptReasons.end())
+                attemptReasons.push_back(message);
+            break;
+
+        case SafeModeTrigger::LevelLoad:
+            levelLoadReasons.push_back(message);
+            break;
+
+        case SafeModeTrigger::None:
+            break;
+
+        case SafeModeTrigger::Custom:
+            break;
+    }
+}
+
+bool SafeMode::shouldDisableLevelProgress()
+{
+    return false; // itzz menu: Safe Mode komplett deaktiviert
+}
+
+ccColor3B SafeMode::getIndicatorColour()
+{
+    if (ForceSafeMode::get()->getRealEnabled())
+        return ccc3(255, 255, 0);
+
+    if (isLevelLoadCheated || isAttemptCheated)
+        return ccc3(255, 0, 0);
+
+    if (PlayLayer::get() && PlayLayer::get()->m_startPosObject)
+        return ccc3(0, 133, 55);
+
+    return ccc3(0, 255, 0);
+}
+
+std::string SafeMode::getCombinedMessages()
+{
+    std::string str = "";
+
+    if (ForceSafeMode::get()->getRealEnabled())
+        str.append("<co>Progress disabled</c> by <cc>safe mode</c>\n");
+
+    if (PlayLayer::get() && PlayLayer::get()->m_startPosObject)
+        str.append("<co>Played level</c> from a <cc>start position</c>\n");
+
+    for (auto reason : levelLoadReasons)
+    {
+        str.append(reason);
+        str.append("\n");
+    }
+
+    for (auto reason : attemptReasons)
+    {
+        str.append(reason);
+        str.append("\n");
+    }
+
+    return str;
+}

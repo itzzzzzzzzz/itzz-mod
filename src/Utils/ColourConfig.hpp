4f5f60dbcd94a3dd1c7560d6a57cbb2908a3e9c8
itzz <georg.ott.mail@gmail.com>
2026-06-20 00:40:04 +0200

    cooking

---
+++ src/Utils/ColourConfig.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+
+enum ColourConfigType
+{
+    CustomColour = 0,
+    Player1 = 1,
+    Player2 = 2,
+    PlayerGlow = 3,
+    Chroma = 4,
+    Pastel = 5,
+    Gradient = 6,
+};
+
+struct ColourConfig
+{
+    struct GradientLocation
+    {
+        cocos2d::ccColor3B colour = cocos2d::ccc3(255, 255, 255);
+        float percentageLocation = 0.0f;
+    };
+
+    cocos2d::ccColor3B customColour = cocos2d::ccc3(255, 255, 255);
+    float opacity = 1.0f;
+    float chromaSpeed = 1.0f;
+    ColourConfigType type = CustomColour;
+    std::vector<GradientLocation> gradientLocations = { { cocos2d::ccc3(255, 0, 0), 0 }, { cocos2d::ccc3(0, 255, 0), 1 } };
+    bool smoothGradient = true;
+    bool inverted = false;
+    bool loopGradient = true;
+
+    cocos2d::ccColor3B colourForConfig(std::string channel);
+    cocos2d::ccColor3B colourForGradient(float v);
+
+    matjson::Value toJson();
+    void fromJson(matjson::Value value);
+};
